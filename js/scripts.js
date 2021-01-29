1e18bf7f5161bd9ac489c44d346e93cfceec4a6a
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-29 10:02:26 -0800

    add extra html elements and JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-function Pizza() {
-  this.size = $("#size").val()
-  this.toppings = $("#toppings").val()
+function Pizza(size, toppings) {
+  this.size = $("#size").val();
+  this.toppings = $("#toppings").val();
+  console.log(this.size);
+  console.log(this.toppings);
 }
 Pizza.prototype.priceCheck = function() {
   //Take total from size and toppings and push to output
 }
+Pizza.prototype.toppingPrice = function() {
+  //Take in all toppings prices, add them up, and push tehm into priceCheck
+}
+let pizzaOne = new Pizza();
+let pizzaTwo = new Pizza();
+
+// - UI LOGIC - //
+
+$(document).ready(function() {
+
+});
