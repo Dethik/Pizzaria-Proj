dfcb5a94d8b4a66bbbf7540580390c0dcf219b3a
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-29 10:48:11 -0800

    scrap html buttons for now, add more constructor logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,38 @@
-function Pizza(size, toppings) {
-  this.size = $("#size").val();
-  this.toppings = $("#toppings").val();
-  console.log(this.size);
-  console.log(this.toppings);
+// BUSINESS LOGIC //
+function Pizza() {
+  this.size = undefined;
+  this.toppings = [];
+  this.sizePrice = 0;
+  this.toppingPrice = 0;
+  this.totalPrice = 0;
 }
+
 Pizza.prototype.priceCheck = function() {
   //Take total from size and toppings and push to output
 }
+
 Pizza.prototype.toppingPrice = function() {
   //Take in all toppings prices, add them up, and push tehm into priceCheck
 }
-let pizzaOne = new Pizza();
-let pizzaTwo = new Pizza();
+
+Pizza.prototype.addSize = function(size) {
+	this.size = size;
+	if (this.size === "small") {
+		this.sizePrice = 12;
+	} else if (this.size === "medium") {
+		this.sizePrice = 18;
+	} else {
+		this.sizePrice = 24;
+	}
+	this.calculateTotal();
+}
+
 
 // - UI LOGIC - //
 
 $(document).ready(function() {
-
+  let pizza = new Pizza();
+  $("#pizza-btn").submit(function(event) {
+    event.preventDefault();
+  });
 });
