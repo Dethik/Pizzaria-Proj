d1a8f6c0417814fc108bc8cf75e3ee3a4411bd5f
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-29 09:34:39 -0800

    add html framework and start of JS constructor

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Pizza() {
+  this.size = $("#size").val()
+  this.toppings = $("#toppings").val()
+}
+Pizza.prototype.priceCheck = function() {
+  //Take total from size and toppings and push to output
+}
